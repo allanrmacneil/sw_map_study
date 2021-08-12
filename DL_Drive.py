18cc220f4db15b1a081f87db7ea623927162ba0b
Allan Macneil <sj919119@reading.ac.uk>
2021-08-12 09:42:11 +0100

    Updated some code and data urls

--- DL_Drive.py
+++ DL_Drive.py
@@ -10,11 +10,13 @@ def DL_Drive(fname): # download a file from my google drive
     
         # match up the file I want to where I've stored in on google drive
         url_dict = {'omni_2006_21.hdf': 'https://drive.google.com/uc?id=1qKdjrF-VQ9wy-Xp1r1X-8OfgSmQV4zWk',
-                    'Matched_crossings_2006_2020.h5':'https://drive.google.com/uc?id=1Lt2456o9zGImAK8hRrvG-nORfXN0ml9U', 
+                    'Matched_crossings_2006_2020_update.h5':'https://drive.google.com/uc?id=1Lt2456o9zGImAK8hRrvG-nORfXN0ml9U', 
                     'HCSGONG_MAPs.hdf':'https://drive.google.com/uc?id=1T_oP3DIva9qHiKdVxG5MPbfCG40Q4tNm', 
-                    'dphi_4d_array.nc':'https://drive.google.com/uc?id=1WDQEuqHmINuTgkqocI6a96GC5BZexQbJ', 
+                    'dphi_4d_array_update.nc':'https://drive.google.com/uc?id=1WDQEuqHmINuTgkqocI6a96GC5BZexQbJ', 
                     'dfss_save_180.hdf':'https://drive.google.com/uc?id=1ilvUJ8E5vgbXPgKpVqs26aArVyphKFmF',
-                   './PFSS_HDFs/pfss_2006-10-07T055400.hdf': 'https://drive.google.com/uc?id=1DEWA2yDKOZt4prRcguL_mYbcNPsxj1IG'}
+                   './PFSS_HDFs/pfss_2006-10-07T055400.hdf': 'https://drive.google.com/uc?id=1DEWA2yDKOZt4prRcguL_mYbcNPsxj1IG',
+                   'Matched_crossings_2006_2020_study.h5': 'https://drive.google.com/file/d/1WDtswUAmB_CQnoVwJnw1O_xuGb1qpmH-',
+                   'dphi_4d_array_study.nc': 'https://drive.google.com/uc?id=1DNn9eSAs0927jDkiJFSrXdeUYvZpQc-Q' }
         url = url_dict[fname] 
         gdown.download(url, fname, quiet=False)
         
